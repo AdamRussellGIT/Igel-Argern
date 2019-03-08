39ced05c30fffcb9bbcab629481e07d888a5cf3e
karolucd <karol.wojcik@ucdconnect.ie>
2019-03-08 14:29:46 +0000

    I added a nice compliment for Adam in main.c

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+//I think blah is a great idea!
 /* 
  * File:   main.c
  * Author: lpasqua
