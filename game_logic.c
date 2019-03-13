0af8c12f2252bbace1559d646cb1e97f5806bce7
AdamRusselGIT <adam.russell@ucdconnect.ie>
2019-03-13 09:45:38 +0000

    Foundations for play_game function!

--- game_logic.c
+++ game_logic.c
@@ -1,6 +1,8 @@
 #include "game_init.h"
 #include "game_logic.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 void printLine();
 
@@ -128,5 +130,107 @@ void place_tokens(square board[NUM_ROWS][NUM_COLUMNS], player players[], int num
  */
 
 void play_game(square board[NUM_ROWS][NUM_COLUMNS], player players[], int numPlayers){
+    //used to control whos turn it is and what names are printed etc
+    int playerTurn = 0;
+    //holds the dice roll number
+    int dice;
+    //variable to check if they want to do the optional move
+    int optionalMove;
     
+    srand(time(NULL));
+    
+    while (/*check win*/)
+    {
+        //we set playerTurn to 0 here so that we get a loop of turns e.g. 1->2->3->1->2->3->1... etc
+        playerTurn = 0;
+        
+        while (playerTurn < numPlayers)
+        {
+            //setting optionalMove to 2 intially so that the while loop where the player chooses whether they want to do the optionalMove enters correctly, see below
+            optionalMove = 2;
+            
+            printf("It is now %s's turn!\n\n", players[playerTurn].playerName);
+            
+            //PART A
+            dice = rand()%6;
+            printf("The dice was rolled, and you got the number (and row) %d!\n\n", dice);
+            
+            
+            //PART B
+            printf("Would you like to do the optional move? (Move one of your tokens 'sideways').");
+            //while loop will enter since optionalMove does not equal 1 or 0
+            while (optionalMove != 0 && optionalMove != 1)
+            {
+                printf("\n(1 for yes, 0 for no): ");
+                scanf("%d", &optionalMove);
+            }
+            
+            if (optionalMove == 1)
+            {
+                //optionalMove code
+            }
+            
+            
+            
+            //PART C
+            printf("\nNow you must select a column which has the token you want to move one space forward!\n");
+            printf("(Remember, you rolled row %d on the dice!)\n", dice);
+            
+            //mandatoryMove code
+            
+            
+            
+            //END OF TURN MISC
+            printf("Here is what the board currently looks like!\n\n");
+            print_board(board);
+            
+        }
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    //while(check for three of same colours in column 8, if yes print player with 3 colours as winner))
+    
+        //counter = 0 here
+           
+            //while(iterate through players counter < numPlayers)
+            //{
+                //PART A
+                //dice role functionality
+    
+                //PART B
+                //ask if want optional move
+                //if they do
+            
+                    //do while: input for row and column
+                    //CHECK OBSTACLE SQUARE TING
+                       //catch for if top of stack of board[row][colum] is not their token
+    
+                     //allow them to select up or down motion
+                        //if row 5 down or row 0 up, allow them,make them select token again
+    
+        
+                //PART C
+                //CHECK IF ROW HAS ANY TOKENS: ASK PASTRAMI
+    
+                //tell them the row they have
+        
+                //do while, until they pick a column that has tokens in it
+                //CHECK OBSTACLE SQUARE TING
+                    //move token at top of stack one column first
+                    //colum that had token moved is set correctly?
+    
+        
+            //print_board(board);
+        //counter++;
+        
+    
+    //}
 }
