d5fbd7325943ac34286a1be4d0a858587db7c983
karolucd <karol.wojcik@ucdconnect.ie>
2019-03-31 19:59:42 +0100

    Game Logic Approach - problem with assigning the color to the board for the mandatory movement

--- game_logic.c
+++ game_logic.c
@@ -2,6 +2,7 @@
 #include "game_logic.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 void printLine();
 
@@ -69,65 +70,59 @@ void printLine(){
  *        players - the array of the players
  *        numPlayers - the number of players  
  */
-void place_tokens(square board[NUM_ROWS][NUM_COLUMNS], player players[], int numPlayers){
-    int minNumOfTokens = 0;
-    int selectedSquare = 0;
+void place_tokens(square board[NUM_ROWS][NUM_COLUMNS], player players[], int numPlayers)
+{
+  //the min number of tokens placed on a square in the first column of the board
+    int minNumOfTokens=0;
+    int selectedSquare=0;
     
-    for (int k=0;k<9;k++)
+    for(int p=0;p<9;p++)
     {
-        for (int l=0;l<6;l++)
+        for(int k=0;k<6;k++)
         {
-            board[k][l].numTokens = 0;
+            board[p][k].numTokens=0;
         }
     }
     
-    for (int i=0;i<4;i++)
-    {
-        for (int j=0;j<numPlayers;j++)
-        {
-            printf("\nPlayer %d please select a square: ", j);
-            scanf("%d", &selectedSquare);
-            
-            printf("Test");
-            
-            while (board[selectedSquare][0].numTokens != minNumOfTokens)
-            {
-                printf("Please select a valid square: ");
-                scanf("%d", &selectedSquare);
-            }
-            
-            if (board[selectedSquare][0].numTokens != 0)
-            {
-                while (board[selectedSquare][0].stack->col == players[j].col)
-                {
-                    printf("Please select a valid square: ");
-                    scanf("%d", &selectedSquare);
-                }
-            }
-            
-            printf("Getting there");
-            
-            board[selectedSquare][0].stack = (token *) malloc(sizeof(token));
-            board[selectedSquare][0].stack->col = players[j].col;
-            board[selectedSquare][0].numTokens++;
-            
-            printf("Placed");
-            
-            //updates the minimum number of Tokens
-            if (((numPlayers * i) + j + 1)%NUM_ROWS == 0)
+    //Add a check for an invalid Row Selection
 
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<numPlayers;j++)
+        {
+                   printf("Player %d please select a square\n",j);
+                   scanf("%d",&selectedSquare);
+
+                while((board[selectedSquare][0].numTokens!=minNumOfTokens))
+                {
+                   printf("Player %d please select a square\n",j);
+                   scanf("%d",&selectedSquare);
+                }
+                if(board[selectedSquare][0].numTokens!=0)
+                {
+                    while(board[selectedSquare][0].stack->col==players[j].col)
+                    {
+                        printf("Player %d please select a valid square\n",j);
+                        scanf("%d",&selectedSquare);
+                    }
+                }
+                
+                
+                board[selectedSquare][0].stack=(token*)malloc(sizeof(token));
+                board[selectedSquare][0].stack->col=players[j].col;
+                board[selectedSquare][0].numTokens++;
+        
+
+            //updates the minimum number of Tokens
+            if(((numPlayers*i)+j+1)%NUM_ROWS==0)
             {
                 minNumOfTokens++;
             }
             
             print_board(board);
         }
-
         
     }
-    
-
-
     
 
 }
@@ -146,68 +141,126 @@ void place_tokens(square board[NUM_ROWS][NUM_COLUMNS], player players[], int num
  */
 
 void play_game(square board[NUM_ROWS][NUM_COLUMNS], player players[], int numPlayers){
-
-    //used to control whos turn it is and what names are printed etc
-    int playerTurn = 0;
-    //holds the dice roll number
-    int dice;
-    //variable to check if they want to do the optional move
-    int optionalMove;
+    
+    int optionMove=2;
+    int tokenToMoveRow=2;
+    int tokenToMoveColu=2;
+    int upOrDown=2;
+    
+    int dice=0;
     
     srand(time(NULL));
+    dice=rand()%6+1;
     
-    //while (/*check win*/)
-    //{
-        //we set playerTurn to 0 here so that we get a loop of turns e.g. 1->2->3->1->2->3->1... etc
-        playerTurn = 0;
-        
-        while (playerTurn < numPlayers)
+    
+    
+    //Add the catches for boundry conditions!
+    //Add the option of choosing the row for the optional and mandatory move
+    for(int c=0;c<numPlayers;c++)//c<4
+    {
+        for(int h=0;h<numPlayers;h++)
         {
-            //setting optionalMove to 2 intially so that the while loop where the player chooses whether they want to do the optionalMove enters correctly, see below
-            optionalMove = 2;
+            printf("Player %s\n",players[h].playerName);
+            printf("Do you wish to Move one of your tokens up or down one row?\n(Press 1 for yes) and (Press 0 for no)\n");
+            scanf("%d",&optionMove);
             
-            printf("It is now %s's turn!\n\n", players[playerTurn].playerName);
-            
-            //PART A
-            dice = rand()%6;
-            printf("The dice was rolled, and you got the number (and row) %d!\n\n", dice);
-            
-            
-            //PART B
-            printf("Would you like to do the optional move? (Move one of your tokens 'sideways').");
-            //while loop will enter since optionalMove does not equal 1 or 0
-            while (optionalMove != 0 && optionalMove != 1)
-            {
-                printf("\n(1 for yes, 0 for no): ");
-                scanf("%d", &optionalMove);
-            }
-            
-            if (optionalMove == 1)
-            {
-                //optionalMove code
-            }
-            
-            
-            
-            //PART C
-            printf("\nNow you must select a column which has the token you want to move one space forward!\n");
-            printf("(Remember, you rolled row %d on the dice!)\n", dice);
-            
-            //mandatoryMove code
-            
-            
-            
-            //END OF TURN MISC
-            playerTurn++;
-            
-            printf("Here is what the board currently looks like!\n\n");
-            print_board(board);
-            
+          if(optionMove==1)
+          {
+             
+             printf("Please select one of your tokens you wish to move\n");
+             
+             printf("Please select the ROW of the token:\n");
+             scanf("%d",&tokenToMoveRow);
+             
+             printf("Please select the COLUMN of the token:\n");
+             scanf("%d",&tokenToMoveColu);
+             
+             while(board[tokenToMoveRow][tokenToMoveColu].stack->col!=players[h].col&&board[tokenToMoveRow][tokenToMoveColu].numTokens==0)
+             {
+               printf("Please select your own token and it's valid token position!\n");
+             }
+             
+             printf("Do you wish to move it UP or DOWN?\n");
+             printf("(Press 1 for UP) and (Press 0 for DOWN)\n");
+             scanf("%d",&upOrDown);
+             if(tokenToMoveRow==0)
+             {
+                 while(upOrDown!=1)
+                 {
+                     printf("You can only move DOWN\nPlease Select DOWN to continue\n");
+                     scanf("%d",&upOrDown);
+                 }
+                 board[tokenToMoveRow+1][tokenToMoveColu].stack->col=players[h].col;
+  
+             }
+             else if(tokenToMoveRow==5)
+             {
+                 while(upOrDown!=0)
+                 {
+                     printf("You can only move DOWN\nPlease Select DOWN to continue\n");
+                     scanf("%d",&upOrDown);
+                 }
+                 board[tokenToMoveRow-1][tokenToMoveColu].stack->col=players[h].col;
+             }
+             else if(upOrDown==1)
+             {
+                 board[tokenToMoveRow-1][tokenToMoveColu].stack->col=players[h].col;
+                 //Get rid of tokenToMove from the original position where it was
+             }
+             else if(upOrDown==0)
+             {
+                 board[tokenToMoveRow+1][tokenToMoveColu].stack->col=players[h].col;
+                 //Get rid of tokenToMove from the original position where it was
+             }
+             print_board(board);
+             
+          }
+          else if(optionMove==0) //Have a check here to make sure there is tokens in the row
+          {
+              printf("The row indicated by the Dice Roll is %d\n\n",dice);
+              print_board(board);
+              printf("Please choose one token (either yours or another player's)\nin the indicated ROW you wish to move forward one square\n");
+              scanf("%d",&tokenToMoveColu);
+              
+              board[dice][tokenToMoveColu++].stack->col=board[dice][tokenToMoveColu].stack->col; //This does not appear to move the selected token as required; the board remains unchanged
+              printf("Movement Right Test\n");
+              print_board(board);
+              tokenToMoveColu=0;
+              
+          }
+             
         }
+    }
+   
+    
         
-        
-        
-    //}
+    
+    
+    //PartA
+    //check while if anyone has won
+    //Check the same for three colors, print winner depending on the color they have chosen to comparing to the three chosen color
+    //counter=0
+    //while counter<numPlayers
+    
+    //Part B 
+    //dice roll functionality
+    //Ask if want option to move his own token 
+    //Ask row and column
+    //Ask up or down
+    //Check for obstacle squares
+    //Catch for out of bounds
+    //Throw back to selection menu for the choosing desired token
+    //if they don't give a valid response ask again
+    
+    //Part C
+    //Check if any tokens in the row selected by dice (ASK THE DOC FOR RULES) re-roll if no tokens in row
+    //Tell the dice roll row to the player
+    //Do while Ask which column then want to make sure there is a token in it
+    //Check for obstacles squares
+    //Move token forward
+    //Make sure the token that had the moved token has the correct token  
+    
+    
     
     
 }
